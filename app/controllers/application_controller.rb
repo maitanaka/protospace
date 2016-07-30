a23506eddab2348754bd11d52eb709564a50a334
MaiTanaka <mai@MacBook-Air-2.local>
2016-07-31 00:33:33 +0900

    add validation and strong parameters to permit the avatar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,9 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << [:nickname, :work, :profile]
+    devise_parameter_sanitizer.for(:sign_up) << [:nickname, :work, :profile, :avatar]
 
-    devise_parameter_sanitizer.for(:account_update) << [:nickname, :work, :profile]
+    devise_parameter_sanitizer.for(:account_update) << [:nickname, :work, :profile, :avatar]
   end
 
 end
