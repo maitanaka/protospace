2c24e2d52e9d9a5316a8ef9384823a7554dbcfc8
MaiTanaka <mai@MacBook-Air-2.local>
2016-07-31 12:36:29 +0900

    generated prototypes_table and model and edited the association between prototypes and users.(has_many and belongs_to)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :prototypes
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -6,6 +7,6 @@ class User < ActiveRecord::Base
 
   validates_presence_of :nickname
 
-    mount_uploader :avatar, AvatarUploader
+  mount_uploader :avatar, AvatarUploader
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160730084725) do
+ActiveRecord::Schema.define(version: 20160731022759) do
+
+  create_table "prototypes", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.integer  "user_id",    limit: 4
+    t.text     "catchcopy",  limit: 65535
+    t.text     "concept",    limit: 65535
+    t.integer  "like_count", limit: 4
+    t.text     "image",      limit: 65535
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
