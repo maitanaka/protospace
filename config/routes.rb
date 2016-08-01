7a18a7e43f44d0df21fcc3a02bcb8a2b9d7fa5b3
MaiTanaka <mai@MacBook-Air-2.local>
2016-08-01 23:19:09 +0900

    added routing for prototypes using resources method

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   root 'prototypes#index'
   resources :users, only: [:edit, :index, :show, :update]
 
-  get  '/prototypes/index'  =>    'prototypes#index'
-  get  '/prototypes/new'    =>    'prototypes#new'
-  get  '/prototypes/show'   =>    'prototypes#show'
+  resources :prototypes, only: [:index, :new, :create, :show]
+
+  # get  '/prototypes/index'  =>    'prototypes#index'
+  # get  '/prototypes/new'    =>    'prototypes#new'
+  # get  '/prototypes/show'   =>    'prototypes#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
