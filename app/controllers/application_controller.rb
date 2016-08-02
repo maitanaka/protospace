bbb0ab6d04e94f0207a8cf59ab6c3e2850f2ea3a
MaiTanaka <mai@MacBook-Air-2.local>
2016-08-03 00:56:46 +0900

    set to use http get method when sign_out and url after sign_out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_out_path_for(resource)
+    user_session_path # サインアウト後のリダイレクト先URL
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << [:nickname, :work, :profile, :avatar]
 
