6daf19dd96fa13668d44d5e2ef10f7c1335706d0
MaiTanaka <mai@MacBook-Air-2.local>
2016-08-04 13:10:00 +0900

    fixed the method

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:edit, :show, :update]
   root to: 'prototypes#index'
-  resources :prototypes, only: [:index, :new, :create, :show]
+  resources :prototypes
 
   # get  '/prototypes/index'  =>    'prototypes#index'
   # get  '/prototypes/new'    =>    'prototypes#new'
