d13888b8a745bf2972b8db1fe5d1aa61557df002
MaiTanaka <mai@MacBook-Air-2.local>
2016-08-04 12:59:25 +0900

    add before action and destroy/edit action

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!
-
+  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :set_prototype, only: [:show, :destroy, :edit, :update]
 
   def index
     @prototypes = Prototype.all
@@ -12,23 +12,42 @@ class PrototypesController < ApplicationController
   end
 
   def create
-      @prototype = Prototype.new(create_params)
+    @prototype = Prototype.new(prototype_params)
     if @prototype.save
-      redirect_to controller: :prototypes, action: :index
+      redirect_to prototypes_path, flash: { success: "Successfully uploaded your prototype!!" }
     else
       render :new
     end
   end
 
   def show
-    @prototype = Prototype.find(params[:id])
     @user = @prototype.user
+  end
+
+  def destroy
+    @prototype.destroy
+    redirect_to root_path, flash: { success: "Deleted your prototype" }
+  end
+
+  def edit
+  end
+
+  def update
+    if @prototype.update(prototype_params)
+      redirect_to prototypes_path, flash: { success: "Updated your prototype" }
+    else
+      render :edit
+    end
   end
 
   private
 
-    def create_params
-      params.require(:prototype).permit(:name, :catchcopy, :concept, images_attributes: [:image_url, :status]).merge(user_id: current_user.id)
+    def prototype_params
+      params.require(:prototype).permit(:name, :catchcopy, :concept, images_attributes: [:id, :image_url, :status]).merge(user_id: current_user.id)
+    end
+
+    def set_prototype
+      @prototype = Prototype.find(params[:id])
     end
 
 end
