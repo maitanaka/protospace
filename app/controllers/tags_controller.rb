614990e1830c42d9f9ee261c9298c1567efb4aa2
MaiTanaka <mai@MacBook-Air-3.local>
2016-08-12 10:00:18 +0900

    add gem kaminari methods to the controller and put them on the view

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,10 +1,10 @@
 class TagsController < ApplicationController
   def index
-    @tags = ActsAsTaggableOn::Tag.most_used
+    @tags = ActsAsTaggableOn::Tag.most_used.page(params[:page]).per(21)
   end
 
   def show
-    @prototypes = Prototype.tagged_with(params[:id])
+    @prototypes = Prototype.tagged_with(params[:id]).page(params[:page]).per(8)
     render template: "prototypes/index"
   end
 end
