ea8e9d1f536e986bccad27742a30ddd13b515694
MaiTanaka <mai@MacBook-Air-3.local>
2016-08-13 14:46:48 +0900

    edit uploader and carrier wave settings to upload images to S3

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -7,9 +7,18 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
   # storage :fog
+  case Rails.env
+    when 'production'
+      storage :fog
 
+    when 'development'
+      storage :fog
+
+    when 'test'
+      storage :file
+  end
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
